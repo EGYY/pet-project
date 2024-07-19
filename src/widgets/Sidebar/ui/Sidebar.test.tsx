fba4da18970bdad0fb1f1900ebaf97d874a4549a
EGYY <pachemuwork@gmail.com>
2024-07-19 11:30:08 +0400

    fix unit tests

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { globalWrapper } from 'shared/lib/tests/globalWrapper';
 import { Sidebar } from './Sidebar';
 
 describe('Test Sidebar widget', () => {
     test('Sidebar render', () => {
-        renderWithTranslation(<Sidebar />);
+        globalWrapper(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Sidebar collapsed', () => {
-        renderWithTranslation(<Sidebar />);
+        globalWrapper(<Sidebar />);
         const sidebar = screen.getByTestId('sidebar');
         const button = screen.getByTestId('sidebar-toggle-button');
         expect(sidebar).toBeInTheDocument();
